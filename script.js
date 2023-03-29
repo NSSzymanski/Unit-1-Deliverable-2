cae0f1d7a6b6122c842296a4b905e5396396c11b
Nell Szymanski <nelliesimone@gmail.com>
2023-03-29 14:23:17 -0400

    commit for resubmission of unit 1 deliverable 2

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ let playerName = prompt ("Welcome to GC mini golf! What is your name?")
 console.log("The player's name is " + playerName)
 
 let numberOfholes = prompt ("Hi, " + playerName + "! Would you like to play 3 or 6 holes?" ) 
- 
+
+let scoreOneOfThree = 0;
+let scoreTwoOfThree = 0;
+let scoreThreeOfThree = 0;
+
+let TotalForThree = 0;
+
+let lessThanNine = 0;
+let OverNine = 0;
+
+let scoreOneOfSix = 0;
+let ScoreTwoOfSix = 0;
+let scoreThreeOfSix = 0;
+let scoreFourOfsix = 0;
+let scoreFiveOfSix = 0;
+let scoreSixOfSix = 0;
+
+let TotalForSix = 0;
+
+let lessThanEighteen = 0;
+let OverEighteen = 0;
+
 
 if (numberOfholes === "3" ) {
     console.log("3 holes it is!");
+
     let scoreOneOfThree = Number( prompt ("How many putts for hole 1? (par:3)"));
         console.log("first hole was " + scoreOneOfThree + " putts");
     let scoreTwoOfThree = Number( prompt ("How many putts for hole 2? (par:3)"));
@@ -16,24 +38,20 @@ if (numberOfholes === "3" ) {
     let TotalForThree = scoreOneOfThree + scoreTwoOfThree + scoreThreeOfThree;
         console.log("The player's total score is " + TotalForThree);
     let lessThanNine = 9 - TotalForThree;
-        console.log("Under par is par minus the score");
     let OverNine = TotalForThree - 9;
-        console.log("over par is the score minus par");
 
         if (TotalForThree === 9) {
-            alert(`Good game, ${playerName} Your total par was: 0`);
-            console.log("The player matches par on three holes if score is 9. This player scored " + TotalForThree);
+            console.log(`Good game, ${playerName}. Your total par was: 0`);
         } else if (TotalForThree < 9) {
-            alert(`Great job, ${playerName}! Your total par was: -` + lessThanNine);
-            console.log("The player is under par on three holes is score is less than 9. This player scored -" + lessThanNine);
+            console.log(`Great job, ${playerName}! Your total par was: -` + lessThanNine);
         } else if (TotalForThree > 9) {
-            alert(`Nice try, ${playerName}... your total par was +` + OverNine);
-            console.log("The player is over par on three holes if they score more than 9. This player scored +" + OverNine);
+            console.log(`Nice try, ${playerName}... Your total par was: +` + OverNine);
         }
         
-
-} else if (numberOfholes === "6" ) {
-    console.log("6 holes it is!")
+     
+    } else if (numberOfholes === "6" ) {
+    console.log("6 holes it is!");
+    
     let scoreOneOfSix = Number( prompt ("How many putts for hole 1? (par:3)"));
         console.log("first hole was " + scoreOneOfSix + " putts");
     let ScoreTwoOfSix = Number( prompt ("How many putts for hole 2? (par:3)"));
@@ -46,23 +64,19 @@ if (numberOfholes === "3" ) {
         console.log("fifth hole was " + scoreFiveOfSix + " putts");
     let scoreSixOfSix = Number( prompt ("How many putts for hole 6? (par:3"));
         console.log("sixth hole was " + scoreSixOfSix + " putts");
-}
+    
 
     let TotalForSix = scoreOneOfSix + ScoreTwoOfSix + scoreThreeOfSix + scoreFourOfsix + scoreFiveOfSix +scoreSixOfSix;
         console.log("The player's total score is " + TotalForSix);
+    
     let lessThanEighteen = 18 - TotalForSix;
-        console.log("Under par is the score minus par");
-    let OverEighteen = 18 - TotalForSix;
-        console.log("over par is par plus the score");
-
+    let OverEighteen = TotalForSix - 18;
+           
         if (TotalForSix === 18) {
-            alert(`Good game, ${playerName} Your total par was: 0`);
-            console.log("The player matches par on 6 holes if score is 18. This player scored" + TotalForSix);
+            console.log(`Good game, ${playerName}. Your total par was: 0`);
         } else if (TotalForSix < 18) {
-            alert(`Great job, ${playerName}! Your total par was: -` + lessThanEighteen);
-            console.log("The player is under par on 6 holes is score is less than 18. This player scored -" + lessThanEighteen);
+            console.log(`Great job, ${playerName}! Your total par was: -` + lessThanEighteen);
         } else if (TotalForSix > 18) {
-            alert(`Nice try, ${playerName}... your total par was +` + OverEighteen);
-            console.log("The player is over par on 6 holes if they score more than 18. This player scored +" + OverEighteen);
+            console.log(`Nice try, ${playerName}... Your total par was: +` + OverEighteen);
         }
-        
+    }
